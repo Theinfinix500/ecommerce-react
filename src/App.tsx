fd91460e5f1f8d2a076d5f168c0d9aa16b2fdcd9
Abdelali Djebara <adjebara@sqli.com>
2023-08-13 15:39:56 +0100

    feat: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      hello this is my ecommerce website
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        className="border shadow p-4 rounded max-w-lg mx-auto mt-56 capitalize text-center bg-blue-400 font-bold text-white"
+      >
+        hello this is my ecommerce website {count}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

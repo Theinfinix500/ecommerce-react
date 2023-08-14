6558cbd6428a8a2526cb39e8e324fdbdc78fd585
Abdelali Djebara <adjebara@sqli.com>
2023-08-14 12:18:52 +0100

    fix: storybook build issue

---
+++ src/components/Example.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Example from "./Example";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Library/Example",
+  component: Example,
+  parameters: {
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Example>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};

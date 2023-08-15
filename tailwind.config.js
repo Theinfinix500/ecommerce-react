97b5acaac0174a0fc064d137284db347a60e7cb5
Abdelali Djebara <adjebara@sqli.com>
2023-08-15 16:20:57 +0100

    feat: category card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      gray: {
-        light: "#F5F6F6",
-        default: "#6F6C6C",
+    extend: {
+      colors: {
+        gray: {
+          light: "#F5F6F6",
+          default: "#6F6C6C",
+        },
+        black: {
+          default: "#181413",
+          darker: "#161414",
+        },
+        green: "#03A703",
       },
-      black: {
-        default: "#181413",
-        darker: "#161414"
-      },
-      green: "#03A703",
     },
-    extend: {},
   },
   plugins: [],
 };

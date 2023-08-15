ab2385f7761d355277b1ba7fcfff8cb857ebba4d
Abdelali Djebara <adjebara@sqli.com>
2023-08-15 23:58:26 +0100

    feat: category search panel

--- src/components/search-category-card/index.tsx
+++ src/components/search-category-card/index.tsx
@@ -4,7 +4,7 @@ interface SearchCategoryCardProps {
 
 const SearchCategoryCard = ({ image }: SearchCategoryCardProps) => {
   return (
-    <div className="w-[270px] h-[80px] bg-gray-light rounded-lg p-3 flex items-center">
+    <div className="min-w-[270px] h-[80px] bg-gray-light rounded-lg p-3 flex items-center">
       <div className="flex items-center gap-3">
         <div className="w-[55px] h-[55px]">
           <img
@@ -15,7 +15,9 @@ const SearchCategoryCard = ({ image }: SearchCategoryCardProps) => {
         </div>
         <div>
           <h1 className="font-bold">Furniture</h1>
-          <span className="text-xs text-gray-default font-semibold">240 Item Available</span>
+          <span className="text-xs text-gray-default font-semibold">
+            240 Item Available
+          </span>
         </div>
       </div>
     </div>
